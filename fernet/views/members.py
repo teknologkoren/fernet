9abcdcdab57b9e0497698c10a2bac8048d1ea8b6
Rupus Reinefjord <rupus@kltrst.se>
2018-02-11 17:36:10 +0100

    ability to select only active members for filter
    backend for selecting arbitrary tag for filters (z-axis for matrices) is
    implemented, but frontend only active as of now

--- fernet/views/members.py
+++ fernet/views/members.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, request, url_for, flash
 from flask_login import login_required
-from wtforms.fields import FormField
+from wtforms.fields import BooleanField, FormField
 from flask_wtf import FlaskForm
 from fernet import app, forms
 from fernet.models import User, Tag
@@ -32,37 +32,49 @@ def all_members():
         tag_dict=tag_dict)
 
 
-@mod.route('/members/<list:tag_list>/')
-def members_by_tags(tag_list):
+@mod.route('/members/<list:tag_list>/', defaults={'mandatory': None})
+@mod.route('/members/<list:tag_list>/all-<list:mandatory>')
+def members_by_tags(tag_list, mandatory):
     """Show active members sorted by the tags in tag_list."""
-    active_users = User.query.filter(User.has_tag('Aktiv'))
+    users = User.query
+    if mandatory:
+        for tag in mandatory:
+            users = users.filter(User.has_tag(tag))
 
     tag_dict = {}
     for tag in tag_list:
-        tag_dict[tag] = active_users.filter(User.has_tag(tag)
-                                            ).order_by(User.first_name)
+        tag_dict[tag] = users.filter(User.has_tag(tag)
+                                     ).order_by(User.first_name)
 
     return render_template(
         'members/members.html',
         tag_list=tag_list,
-        tag_dict=tag_dict)
+        tag_dict=tag_dict,
+        mandatory=mandatory)
 
 
-@mod.route('/members/<list:columns>/<list:rows>/')
-def member_matrix(columns, rows):
+@mod.route('/members/<list:columns>/<list:rows>/', defaults={'mandatory': None})
+@mod.route('/members/<list:columns>/<list:rows>/all-<list:mandatory>')
+def member_matrix(columns, rows, mandatory):
     """Show a matrice of members based on their tags."""
+    users = User.query
+    if mandatory:
+        for tag in mandatory:
+            users = users.filter(User.has_tag(tag))
+
     tag_dict = {}
     for column in columns:
         tag_dict[column] = {}
         for row in rows:
-            tag_dict[column][row] = User.query.filter(
-                    User.has_tag(column),
-                    User.has_tag(row))
+            filtered = users.filter(User.has_tag(column), User.has_tag(row))
+
+            tag_dict[column][row] = filtered
 
     return render_template('members/member_matrix.html',
                            columns=columns,
                            rows=rows,
-                           tag_dict=tag_dict)
+                           tag_dict=tag_dict,
+                           mandatory=mandatory)
 
 
 @mod.route('/members/filter/', methods=['GET', 'POST'])
@@ -75,19 +87,26 @@ def filter_members():
 
     F.col_form = FormField(TagColForm)
     F.row_form = FormField(TagRowForm)
+    F.only_active = BooleanField('Only active members', default=True)
 
     form = F()
 
     if form.validate_on_submit():
         if form.col_form.checked_tags():
+            if form.only_active.data:
+                mandatory = ['Aktiv']
+            else:
+                mandatory = None
 
             if form.row_form.checked_tags():
                 return redirect(url_for('.member_matrix',
                                         columns=form.col_form.checked_tags(),
-                                        rows=form.row_form.checked_tags()))
+                                        rows=form.row_form.checked_tags(),
+                                        mandatory=mandatory))
 
             return redirect(url_for('.members_by_tags',
-                                    tag_list=form.col_form.checked_tags()))
+                                    tag_list=form.col_form.checked_tags(),
+                                    mandatory=mandatory))
         else:
             flash("Please enter which tags to display (in columns)", 'error')
 
@@ -100,13 +119,4 @@ def voices():
     """Show members by voice."""
     tag_list = ['Sopran 1', 'Sopran 2', 'Alt 1', 'Alt 2', 'Tenor 1',
                 'Tenor 2', 'Bas 1', 'Bas 2']
-    return members_by_tags(tag_list)
-
-
-@mod.route('/members/groups/')
-def groups():
-    """Show members by group."""
-    columns = ['Sånggrupp 1', 'Sånggrupp 2', 'Sånggrupp 3']
-    rows = ['Sopran 1', 'Sopran 2', 'Alt 1', 'Alt 2', 'Tenor 1', 'Tenor 2',
-            'Bas 1', 'Bas 2']
-    return member_matrix(columns, rows)
+    return members_by_tags(tag_list, mandatory=['Aktiv'])
